a543f8a42ec3b1374602fb4eb7d6b8de00d165c9
Garrit Franke <garritfranke@gmail.com>
2020-11-05 20:44:20 +0100

    Add simple server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::io::Read;
+use std::io::Write;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    // 1965 is the standard port for gemini
+    let port = "1965";
+    let address = format!("0.0.0.0:{}", port);
+    let listener =
+        TcpListener::bind(address).unwrap_or_else(|_| panic!("Could not bind to port {}", port));
+
+    println!("Listening on port 1965");
+
+    for stream in listener.incoming() {
+        if let Ok(mut stream) = stream {
+            let mut buffer = [0; 1024];
+            if let Err(e) = stream.read(&mut buffer) {
+                println!("Could not read from stream: {}", e)
+            }
+
+            if let Err(e) = stream.write(b"HELLO") {
+                println!("Could not write to stream: {}", e);
+            }
+        }
+    }
 }
