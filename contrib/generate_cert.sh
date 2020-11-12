71a6eb03e6983c2f81c2037bcdd9e3429f1bde56
Garrit Franke <garritfranke@gmail.com>
2020-11-12 13:29:15 +0100

    Use 4096 bit key length for test certs
    Fixes a security error on debian machines

--- contrib/generate_cert.sh
+++ contrib/generate_cert.sh
@@ -3,7 +3,7 @@
 # When prompted for multiple lines of information, leave everything blank instead of "common name"
 # This should be your domain name. E.g. "localhost" if you are testing on your local machine
 
-openssl genrsa -des3 -out server.key 1024
+openssl genrsa -des3 -out server.key 4096
 openssl req -new -key server.key -out server.csr
-openssl x509 -req -days 1024 -in server.csr -signkey server.key -out server.crt
+openssl x509 -req -days 4096 -in server.csr -signkey server.key -out server.crt
 openssl pkcs12 -export -out identity.pfx -inkey server.key -in server.crt
